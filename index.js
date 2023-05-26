ea14f1f8ed2a5fcf7cca4eda6e1d286d1a7103ac
CHONG Jong Hoa <chongjonghoa@MBP-de-CHONG.lan>
2023-05-26 19:05:54 +0200

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const mongoose = require('mongoose');
-const { database } = require('./config');
-require('dotenv').config();
+const mongoose = require("mongoose");
+const { database } = require("./config");
 
 // Middleware
 app.use(express.json());
@@ -12,17 +11,20 @@ app.use(express.json());
 app.use(cors());
 
 // Connexion à la base de données MongoDB
-mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connexion à la base de données MongoDB établie');
+    console.log("Connexion à la base de données MongoDB établie");
   })
   .catch((error) => {
-    console.error('Erreur de connexion à la base de données MongoDB:', error);
+    console.error("Erreur de connexion à la base de données MongoDB:", error);
   });
 
+require("dotenv").config();
+
 // Définition des routes de l'API
-const pangolinRoutes = require('./routes/pangolin');
-app.use('/', pangolinRoutes);
+const pangolinRoutes = require("./routes/pangolin");
+app.use("/", pangolinRoutes);
 
 // Démarrage du serveur
 app.listen(process.env.PORT, () => {
